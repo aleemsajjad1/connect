b0bf3b9ae8461b53e558ebf7d725763d39dd7155
unknown <aleemsajjad1@gamil.com>
2021-01-14 14:59:13 +0500

    new build

---
+++ precache-manifest.0e8569e32054fee5de73bbb9c9b6d588.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d24442904c9b0fe614e7331274ed225",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3686be9ae8334c918ec",
+    "url": "/static/css/2.064924bc.chunk.css"
+  },
+  {
+    "revision": "713b3e65068c05d4aabd",
+    "url": "/static/css/main.0cc18fa5.chunk.css"
+  },
+  {
+    "revision": "f3686be9ae8334c918ec",
+    "url": "/static/js/2.aeb6205f.chunk.js"
+  },
+  {
+    "revision": "2472699aaa10b9a72af073dce626f180",
+    "url": "/static/js/2.aeb6205f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "713b3e65068c05d4aabd",
+    "url": "/static/js/main.3d7d9d85.chunk.js"
+  },
+  {
+    "revision": "0316996d9df51436b381",
+    "url": "/static/js/runtime-main.637ec23c.js"
+  },
+  {
+    "revision": "1e140a7ca06488636c151628d26a1b0e",
+    "url": "/static/media/2.1e140a7c.png"
+  },
+  {
+    "revision": "08926ebedc29b293ab635904f8b6b8ad",
+    "url": "/static/media/AppStore.08926ebe.png"
+  },
+  {
+    "revision": "12cb447ace54f262507f37a6aa224a6e",
+    "url": "/static/media/BillingPage.12cb447a.png"
+  },
+  {
+    "revision": "b279934ec3a15a759f261ba6a60fce01",
+    "url": "/static/media/Capture.b279934e.PNG"
+  },
+  {
+    "revision": "f186bf3cfc705548b2dea2abc184a684",
+    "url": "/static/media/SoConnect_ROUNDED.f186bf3c.png"
+  },
+  {
+    "revision": "725d900b8a13519f496f20a8cfc94149",
+    "url": "/static/media/card.725d900b.png"
+  },
+  {
+    "revision": "68903800d273f9af92c09c807bbe9e82",
+    "url": "/static/media/lock-gray.68903800.png"
+  },
+  {
+    "revision": "94fee3eb68bc07331104cc9b43b3ddaf",
+    "url": "/static/media/logo.94fee3eb.png"
+  },
+  {
+    "revision": "593b5b45537fa2776005c5b9c12aafd1",
+    "url": "/static/media/main.593b5b45.png"
+  },
+  {
+    "revision": "20453ff3625e0cc491021401f5b1237a",
+    "url": "/static/media/print.20453ff3.png"
+  },
+  {
+    "revision": "89b59ab8e35084110385f43149ff3c49",
+    "url": "/static/media/right-mark.89b59ab8.png"
+  },
+  {
+    "revision": "293e5c29304e2e3a8c6a03b52fe24e61",
+    "url": "/static/media/web.293e5c29.png"
+  }
+]);
